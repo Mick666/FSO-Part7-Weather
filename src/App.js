962ec1981bbe007b9d734b177bc45363d51a60e3
Michael <Mick666@hotmail.com.au>
2020-09-20 19:26:28 +1000

    Finished exercise

--- src/App.js
+++ src/App.js
@@ -18,7 +18,30 @@ const useField = (type) => {
 const useCountry = (name) => {
   const [country, setCountry] = useState(null)
 
-  useEffect()
+  useEffect(() => {
+    if (name === '') return
+    axios
+    .get(`https://restcountries.eu/rest/v2/name/${name}?fullText=true`)
+    .then(response => {
+      if (response.data.length !== 1) {
+        let responseData = {
+          found: false,
+          data: null
+        }
+        setCountry(responseData)
+      } else {
+        let responseData = {
+          found: true,
+          data: response.data[0]
+        }
+        setCountry(responseData)
+      }
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  },
+  [name])
 
   return country
 }
